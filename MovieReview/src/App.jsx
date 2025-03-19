a44a7594c953424bb7766ebfdc71f869936c612a
unknown <ismasenlimbu@gmail.com>
2025-03-19 15:35:28 +0545

    Added a navbar to main page

--- MovieReview/src/App.jsx
+++ MovieReview/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/navbar'
+import home from './pages/Home'
+import about from './pages/About'
+import movie from './pages/Movie'
+import journal from './pages/Journal'
+import { Route,Routes, BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Router>
+        <Navbar/>
+        <div>
+        <Routes>
+          <Route exact path="/" component={home}></Route>
+          <Route path="/about" component={about}></Route>
+          <Route path="/movie" component={movie}></Route>
+          <Route path="/journal" component={journal}></Route>
+        </Routes>
+          
+        </div>
+      </Router>
+    </div>
   )
 }
 
